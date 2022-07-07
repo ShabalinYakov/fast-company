31c13df3e721dde89ec1de97f66ad9760e129207
Yakov Shabalin <shab.yakov@ya.ru>
2022-07-07 22:18:46 +0500

    implemented table layout, padding and styles

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import api from '../api';
 
 const Users = () => {
-  console.log(api.users.fetchAll());
+  const [users, setUsers] = useState(api.users.fetchAll());
+  const handleDelete = (userId) => {};
 
-  return <h1>Users</h1>;
+  const renderPhrase = (number) => {};
+  return (
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((qualities) => (
+                    <span
+                      key={qualities._id}
+                      className={`badge bg-${qualities.color} m-1`}
+                    >
+                      {qualities.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{`${user.rate} /5`}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(user._id)}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Users;
