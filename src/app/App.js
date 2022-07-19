733d09edc91583fe60afc2afa8ed1fd5c5c15ee4
Shabalin_Yakov <shab.yakov@ya.ru>
2022-07-19 19:54:16 +0500

    added PropTypes

--- src/app/App.js
+++ src/app/App.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react';
-import Users from './components/users';
-import SearchStatus from './components/searchStatus';
-import api from './api';
+import React, { useState } from "react";
+import Users from "./components/users";
+import SearchStatus from "./components/searchStatus";
+import api from "./api";
 
 const App = () => {
-  const [users, setUsers] = useState(api.users.fetchAll());
+    const [users, setUsers] = useState(api.users.fetchAll());
 
-  const handleDelete = (userId) => {
-    setUsers(users.filter((user) => user._id !== userId));
-  };
-  const handleToggleBookMark = (id) => {
-    const newUsers = users.map((user) => {
-      if (user._id === id) {
-        return {
-          ...user,
-          bookmark: !user.bookmark,
-        };
-      }
-      return user;
-    });
-    setUsers(newUsers);
-  };
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId));
+    };
+    const handleToggleBookMark = (id) => {
+        const newUsers = users.map((user) => {
+            if (user._id === id) {
+                return {
+                    ...user,
+                    bookmark: !user.bookmark
+                };
+            }
+            return user;
+        });
+        setUsers(newUsers);
+    };
 
-  return (
-    <div>
-      <SearchStatus length={users.length} />
-      <Users
-        users={users}
-        onDelete={handleDelete}
-        onToggleBookMark={handleToggleBookMark}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <SearchStatus length={users.length} />
+            <Users
+                users={users}
+                onDelete={handleDelete}
+                onToggleBookMark={handleToggleBookMark}
+            />
+        </div>
+    );
 };
 
 export default App;
--- src/app/components/user.jsx
+++ src/app/components/user.jsx
@@ -1,42 +1,57 @@
-import Qualitie from './qualitie';
-import BookMark from './bookmark';
+import React from "react";
+import Qualitie from "./qualitie";
+import BookMark from "./bookmark";
+import PropTypes from "prop-types";
 
-const User = ({ user, ...rest }) => {
-  return (
-    <>
-      <tr key={user._id}>
-        <td>{user.name}</td>
-        <td>
-          {user.qualities.map((item) => (
-            <Qualitie
-              key={item._id}
-              color={item.color}
-              name={item.name}
-              id={item._id}
-            />
-          ))}
-        </td>
-        <td>{user.profession.name}</td>
-        <td>{user.completedMeetings}</td>
-        <td>{user.rate} /5</td>
-        <td>
-          <BookMark
-            status={user.bookmark}
-            id={user._id}
-            onToggleBookMark={rest.onToggleBookMark}
-          />
-        </td>
-        <td>
-          <button
-            onClick={() => rest.onDelete(user._id)}
-            className="btn btn-danger"
-          >
-            delete
-          </button>
-        </td>
-      </tr>
-    </>
-  );
+const User = ({
+    _id,
+    name,
+    qualities,
+    profession,
+    completedMeetings,
+    rate,
+    onDelete,
+    bookmark,
+    onToggleBookMark
+}) => {
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>
+                {qualities.map((qual) => (
+                    <Qualitie key={qual._id} {...qual} />
+                ))}
+            </td>
+            <td>{profession.name}</td>
+            <td>{completedMeetings}</td>
+            <td>{rate} /5</td>
+            <td>
+                <BookMark
+                    status={bookmark}
+                    onClick={() => onToggleBookMark(_id)}
+                />
+            </td>
+            <td>
+                <button
+                    onClick={() => onDelete(_id)}
+                    className="btn btn-danger"
+                >
+                    delete
+                </button>
+            </td>
+        </tr>
+    );
 };
-
+User.propTypes = {
+    user: PropTypes.array,
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    qualities: PropTypes.array.isRequired,
+    profession: PropTypes.object.isRequired,
+    rate: PropTypes.number.isRequired,
+    bookmark: PropTypes.bool.isRequired,
+    completedMeetings: PropTypes.number.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired
+};
 export default User;
