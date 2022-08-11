21c6dab8acbd20ab97913eb08b4c039c51fa85d5
Shabalin_Yakov <shab.yakov@ya.ru>
2022-08-11 17:25:53 +0500

    added component NavBar

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Users from "./components/users";
 
 function App() {
-    return <Users />;
+    return (
+        <>
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/login" component={Login} />
+                <Route path="/users" component={Users} />
+            </Switch>
+        </>
+    );
 }
 
 export default App;
