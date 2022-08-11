f986a60109a8fdd834d9030c50d92fb0f72f2a9c
Shabalin_Yakov <shab.yakov@ya.ru>
2022-08-11 17:32:04 +0500

    added component Main and Login

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/navBar";
 import Users from "./components/users";
+import Main from "./components/main";
+import Login from "./components/login";
 
 function App() {
     return (
         <>
+            <NavBar />
             <Switch>
                 <Route exact path="/" component={Main} />
                 <Route path="/login" component={Login} />
