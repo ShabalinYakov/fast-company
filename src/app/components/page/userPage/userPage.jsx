07d477eefa7c9ccae83df855d0791220dfdf9329
Shabalin_Yakov <shab.yakov@ya.ru>
2022-10-31 10:39:39 +0500

    created component QualitiesCard

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import QualitiesCard from "../../ui/qualitiesCard";
 import UserCard from "../../ui/userCard";
 import PropTypes from "prop-types";
 import api from "../../../api";
@@ -16,6 +17,7 @@ const UserPage = ({ id }) => {
                 <div className="row gutters-sm">
                     <div className="col-md-4 mb-3">
                         <UserCard user={user} />
+                        <QualitiesCard data={user.qualities} />
                     </div>
                     <div className="col-md-8">Comments</div>
                 </div>
